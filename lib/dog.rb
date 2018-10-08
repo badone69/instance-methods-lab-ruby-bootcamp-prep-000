0a1910a9d151d7006da93452afcacc866d7e9059
student online <eliel.badon@gmail.com>
2018-10-08 22:08:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,6 +3,17 @@ class Dog
     puts "Woof!"
   end
   def sit 
-    puts
+    puts "The Dog is sitting"
+  end 
 end 
+
+class Person
+  def talk 
+    puts "Hello World!"
+  end 
+  def walk
+    puts "The Person is walking"
+  end 
+end 
+
 
